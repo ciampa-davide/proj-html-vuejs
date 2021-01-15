f245a00d51adf8bd1e5815bf02dc7fa63293cb57
Davide <ciampad06@gmail.com>
2021-01-15 14:47:56 +0100

    fine prima parte vuejs

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var app = new Vue(
+  {
+    el:"#root",
+    data: {
+      links: [
+        "HOME",
+        "MISSION",
+        "CAUSES",
+        "JOURNAL"
+      ]  
+    }
+  }
+)
+// <i class="fab fa-instagram"></i>
